ed42fb7907c2e2861b764c5af3e0a13215c8e44e
rreys <roger.reyesm97@gmail.com>
2024-05-01 18:21:00 -0500

    Actualizacion de plantilla con js

---
+++ asset/script/content.js
@@ -0,0 +1,125 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // console.log("dict: "+ dict["intro"]["title"])
+    intro();
+    infoAboutMe();
+    skill();
+    project();
+    contact();
+});
+
+function intro(){
+    let intro = '';
+    intro += "<h3 class='text-end'><a href='#intro'>"+ dict.intro.title +"</a></h2>";
+    intro += "<p class='text-end'>"+ dict.intro.description +"</p>";
+    // Agregar el HTML al div con id "content"
+    document.getElementById('intro').innerHTML = intro;
+}
+
+function infoAboutMe(){
+    let text_inf = `<div class='container text-center'> 
+        <h3 class='text-center'><a href='#AboutMe'>${dict.aboutMe.title}</a></h2>
+        <div>${dict.aboutMe.description}</div>
+        <img class='img-fluid' src="${dict.aboutMe.img}"/>
+    </div>`;
+        
+    let infoAboutMe = document.getElementsByClassName('infoAboutMe')[0];
+    infoAboutMe.innerHTML = text_inf
+}
+
+function skill(){
+    let content = '';
+
+    let text = `<div class='container'>`;
+    text = `<h3 class="text-center"><a href="#AboutMe">${dict["skill"]["title"]}</a></h2>`;
+    text += `<div class="container text-center">`
+    
+    for(var key in dict.skill){
+        if (dict.skill.hasOwnProperty(key)) {
+            
+            var aRow = (parseInt(key)%2 == 0);
+            var item = dict.skill[key];
+            
+            content += `<div class="col-6">
+                <h3>${item.title}</h3>
+                <ul><li>${item.description}</li></ul>
+            </div>`;              
+            
+
+            text += aRow ? '<div class="row">' + content + `</div>`:'';
+            content = aRow ? '': content;
+        }
+    }
+    text += '</div></div>';
+    
+    let skill = document.getElementsByClassName('skill')[0];
+    skill.innerHTML = text
+}
+
+function project(){
+    let text = '';
+    let active = true;
+    text += `<h2><a href="#Project">${dict.project.title}</a></h2>`;
+
+    text += `<div id="carouselExampleCaptions" class="carousel carousel-dark slide" data-bs-ride="carousel">
+    <div class="carousel-inner">`;
+    for(var key in dict.project){
+        
+        if (dict.project.hasOwnProperty(key) && (parseInt(key)%1)==0) {
+            var item = dict.project[key];
+            
+            text += `<div class="carousel-item ${ (active) ? 'active' : '' }">
+                <img src="${item.img}" class="d-block mx-auto w-100" alt="${item.alt}">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="${item.color ? item.color : 'text-dark'} d-block">${item.title}</h5>
+                    <p class="${item.color ? item.color : 'text-dark'}">${item.description}</p>
+                    <a href="${item.link}" class="btn btn-dark stretched-link" target="_blank">Ver pagina</a>
+                </div>
+            </div>`;
+            active = false;
+        }
+    }
+    text += `</div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>`;
+    text += `<p>Más proyectos próximamente...</p>`
+
+    let project = document.getElementById('project');
+    project.innerHTML = text
+}
+
+function contact(){
+    let text = `<div class='container text-center'>`;
+    text += `<h2><a href="#Contact">${dict.contact.title}</a></h2>`;
+    text += `<div class="container text-center">`
+    text += `<div class="row">`;
+    
+    for(var key in dict.contact){
+        if (dict.contact.hasOwnProperty(key) && (parseInt(key)%1)==0) {
+            
+            var item = dict.contact[key];
+            
+            text += `<div class="col">
+            <nav>
+                <ul>
+                    <li><a target="_blank" href="${item.link}">
+                            <img src="${item.img}" alt="${item.page}"><br>
+                            <p>${item.nickname}</p>
+                        </a>
+                    </li>                      
+                </ul>
+            </nav>
+        </div>`;              
+        }
+    }
+    text += '</div></div></div>';   
+
+    let contact = document.getElementById('contact');
+    contact.innerHTML = text
+}
---
+++ asset/script/env.js
@@ -0,0 +1,93 @@
+var dict = new Object();
+
+var dict = {
+    "intro":{
+        "title":"Desarrollador",
+        "description":"Cumpliendo objetivos mutuos"
+    },
+    "aboutMe":{
+        "title":"Acerca de mi",
+        "description":"<p>¡Hola! Soy Roger Reyes, un apasionado desarrollador en constante crecimiento y siempre en busca de nuevos desafíos. Mi objetivo principal es utilizar mis habilidades para desarrollar proyectos innovadores que resuelvan problemas y brinden soluciones efectivas.</p><p>Creo firmemente en el poder del trabajo en equipo y estoy entusiasmado por colaborar con otros profesionales para alcanzar metas comunes.</p>",
+        "img":"asset/img/perfil.jpg"
+    },
+    "skill":{
+        "title":"Habilidades",
+        "1":{
+            "title":"Lenguajes de Marcado",
+            "description":"CSS Grid / Flex, HTML"
+        },
+        "2":{
+            "title":"Lenguajes de programación",
+            "description":"PHP, JavaScript, Python, C#, Java EE"
+        },
+        "3":{
+            "title":"Base de datos",
+            "description":"SQL / MySQL server, Mongoos"
+        },
+        "4":{
+            "title":"Librerias",
+            "description":"Bootstrap, jQuery"
+        },
+        "5":{
+            "title":"Framework",
+            "description":"node JS, Express, Laravel, Angular"
+        },
+        "6":{
+            "title":"Sistemas Operativos",
+            "description":"Windows Server, CentOS, Ubuntu"
+        }
+    },
+    "project":{
+        "title":"Proyectos destacados",
+        "1":{
+            "title":"Tienda online",
+            "description":"Uso de CSS, HTML, PHP (POO y MVC) y SQL.",
+            "link":"https://tiendaonliphppoomvc.rogerryes.repl.co/",
+            "img":"asset/img/tiendaOnline.png",
+            "alt":"tiendaOnline"
+        },
+        "2":{
+            "title":"Bloc",
+            "description":"Uso de Bootstrap, jQuery, responsive design.",
+            "link":"https://proyectojsjquery.rogerryes.repl.co/index.html",
+            "img":"asset/img/js_jquery.png",
+            "alt":"bloc",
+            "color": "text-white"
+        },
+        "3":{
+            "title":"Geolocalizador de dispositivos",
+            "description":"Uso de CSS, HTML, JS, PHP, JSON, Mapbox y SQL.",
+            "link":"https://proyectojsjquery.rogerryes.repl.co/index.html",
+            "img":"asset/img/rastreador.png",
+            "alt":"rastreador"
+        }
+
+    },
+    "contact":{
+        "title":"Contactame",
+        "1":{
+            "link":"https://www.linkedin.com/in/rogerreys/",
+            "img":"asset/img/linkedIn.png",
+            "page":"linkedin",
+            "nickname":"\\rogerreys"
+        },
+        "2":{
+            "link":"https://github.com/rogerreys",
+            "img":"asset/img/github.png",
+            "page":"github",
+            "nickname":"\\rogerreys"
+        },
+        "3":{
+            "link":"url",
+            "img":"asset/img/gmail.png",
+            "page":"gmail",
+            "nickname":"r_reys"
+        },
+        "4":{
+            "link":"https://twitter.com/roger_reys",
+            "img":"asset/img/twitter.png",
+            "page":"twitter",
+            "nickname":"roger_reys"
+        }
+    }
+}
