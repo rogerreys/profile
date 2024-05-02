d8c062f41517415d716779fd2982c2ad31cd91d4
Roger Reyes <rreyes@cobiscorp.com>
2024-05-01 23:57:37 -0500

    Ajuste en scroll y correccion con dimensiones screen

--- asset/script/dimensiont_windows.js
+++ asset/script/dimensiont_windows.js
@@ -1,9 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    screenSize();
 
-// Obtener el ancho y el largo de la pantalla
-var widthScreen = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-var heightScreen = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+});
+
+function screenSize() {
+    // Obtener el ancho y el largo de la pantalla
+    var widthScreen = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var heightScreen = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
 
-console.log(widthScreen + "px " + heightScreen + "px");
-document.documentElement.style.setProperty("--var-width", widthScreen - 17 + "px");
-document.documentElement.style.setProperty("--var-height", heightScreen + "px");
+    console.log(widthScreen + "px " + heightScreen + "px");
+    document.documentElement.style.setProperty("--var-width", widthScreen - 5 + "px");
+    document.documentElement.style.setProperty("--var-height", heightScreen + "px");
+}
